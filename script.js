c72c3d4845b087171734b71e2ab2fe543978261f
Ana Beatriz Do Vale Bezerra <148078475+Anabiaaa23@users.noreply.github.com>
2025-06-05 16:30:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,40 @@ document.getElementById('pedidoForm').addEventListener('submit', function(e) {
   const endereco = document.getElementById('endereco').value.trim();
   const email = document.getElementById('email').value.trim();
 
+
+
   if (!nome || !telefone || !peca || !endereco || !email) {
     alert('Por favor, preencha todos os campos!');
+    return;
+  }
+
+  if (nome.length < 10) {
+    alert('Por favor, escreva seu nome e sobrenome!');
     return;
   }
 
   const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailValido.test(email)) {
     alert('Por favor, insira um e-mail válido!');
+    return;
+  }
+
+ 
+  const telefoneValido = /^\d{8,}$/;
+  if (!telefoneValido.test(telefone)) {
+    alert('Por favor, insira um telefone válido!');
+    return;
+  }
+
+
+  if (peca.length < 5) {
+    alert('Por favor, descreva melhor seu pedido!');
+    return;
+  }
+
+
+  if (endereco.length < 10) {
+    alert('Por favor, insira um endereço válido!');
     return;
   }
 
